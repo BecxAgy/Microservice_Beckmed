f29caeba924b66e75dacaeced6f11fb360146587
Rebeca <beca.cout@gmail.com>
2023-12-06 21:58:09 -0300

    chore: packge

--- ms-patient/src/main/java/com/ms/mspatient/config/CorsConfig.java
+++ ms-patient/src/main/java/com/ms/mspatient/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.ms.doctor.config;
+package com.ms.mspatient.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
